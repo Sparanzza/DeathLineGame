918e6dfea141f08cc120ac29934c947fc160f1aa
Sparanzza <aurelio@sparanzza.com>
2017-10-26 00:44:33 +0200

    alpha and send to back the background image

--- Background.lua
+++ Background.lua
@@ -2,8 +2,9 @@
 
 function newBackground()
 	self = display.newImage( "Bacground.jpg" )
+	self.alpha = 0.2  --change the alpha property
 	self.x = display.contentWidth / 2
 	self.y = display.contentHeight / 2
-
+	self:toBack( )
 	return self
 end
