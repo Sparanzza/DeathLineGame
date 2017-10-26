4f9c5181fac03ace9950774eb66ed00778543821
Sparanzza <aurelio@sparanzza.com>
2017-10-26 17:19:05 +0200

    gradiente
    effect fade ramp for objects

--- utilsFx.lua
+++ utilsFx.lua
@@ -11,5 +11,11 @@ function bloomFx( object )
 	object.fill.effect.blur.horizontal.sigma = 140
 	object.fill.effect.blur.vertical.blurSize = 20
 	object.fill.effect.blur.vertical.sigma = 240
+end
+
+function centerGradient( object )
+	object.fill.effect = "filter.vignetteMask"
+	object.fill.effect.innerRadius = 0.2
+	object.fill.effect.outerRadius = 0.9
 
 end
