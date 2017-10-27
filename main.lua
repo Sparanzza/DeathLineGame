fec1ee81e6925c1e806eaa71adb631b644bd2fbb
Sparanzza <aurelio@sparanzza.com>
2017-10-27 11:04:54 +0200

    main and title Screen
    -Add require titleScreen.lua in main file.
    -Add function update the game --- update the velocity and score
    -add function endLevel, to generate a object from class titleScreen at
    the end of the game!

--- main.lua
+++ main.lua
@@ -15,7 +15,7 @@ require("Player") --add player file
 require("GameTrack") -- add game track file
 require("Background") -- add background file
 require("Foreground") --add foreground file
---require("TitleScreen") -- add Title Screen file
+require("TitleScreen") -- add Title Screen file
 require("Utilities") -- add utilities file
 
 gameFxGrp = display.newGroup( )
@@ -39,25 +39,57 @@ function load() --starting function, load object
 	printd("creating player ...")
 	player = newPlayer() -- create new Player
 	foreground = newForeground()
+	levelDebugText = display.newText("",200,64)
 	printd("starting game ...")
 	newGame() --start the game
 	printd("end game ...")
 end
-function updateEngine( event )
-	-- body
+
+function update(e)
+	if leftIsHeld then
+		player:changeVelocity(-.65)
+	end
+	if rightIsHeld then
+		player:changeVelocity(.65)
+	end
+	level:updateLocation()
+	player:updateLocation()	
+	if isActive then
+		player:scoreDiference(level.xScore)
+		if player.xDiff < 100 then 
+			local newPoints = ((100-player.xDiff)*(100-player.xDiff))/10
+			-- levelDebugText.text = newPoints ..  ", " .. player.xDiff
+			score = score + newPoints
+		end
+		foreground:updateUI()
+	end
 end
 
 function newGame( )
-
+	--display.setDefault( "background", .1, .1, .1  )
 	gameFxGrp:insert(player)
 	gameFxGrp:insert(level)
-	socre = 0 --reset score
+	score = 0 --reset score
 	isActive = false --set active whether the game is false
 	--player:updateLocation() --update player location
 	player:reset() --reset player
 	level:addLines() --add lines to the level
 	level:start( ) --start the level
 	foreground:updateUI()
+	timer.performWithDelay( 3000+gameLength*1000, endLevel )
+	Runtime:addEventListener( "enterFrame", update )
+
+end
+
+function endLevel()
+	if score > highScore then
+		highScore = score
+	end
+	Runtime:removeEventListener( "enterFrame", update )
+	player:reset()
+	level:reset()
+	foreground:updateUI()
+	titleScreen = newTitleScreen()
 
 end
 
